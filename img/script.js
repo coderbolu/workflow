117963784e45d583a412bbd1d54d32310c0dec3c
coderbolu <boluwatifetoluwalase13@gmail.com>
2025-04-29 20:43:27 -0700

    js

--- img/script.js
+++ img/script.js
@@ -8,6 +8,14 @@ Mytitle
 _job
 
 
-null // string
+null // string is used to leave comment on js
 
 console.log (typeof myname)
+
+    ///basic operators
+    //+-/*
+
+    const now =2039
+
+    const ageJack = now - 1991
+    console.log(ageJack)
