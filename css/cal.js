529418aa1f0647cbb1f1fcb1391970a085c0a3f1
coderbolu <boluwatifetoluwalase13@gmail.com>
2025-05-06 20:32:21 -0700

    cal-js

---
+++ css/cal.js
@@ -0,0 +1,6 @@
+const tipButton = document.querySelectorAll(".tip-button");
+const customTip = document.getElementById("custom-tip")
+const peopleInput = document.getElementById("people")
+const billInput = document.getElementById("bill")
+const resetButton = document.getElementById("reset")
+
