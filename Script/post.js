5a40295f820aebb4a48122926f2887656d296463
coderbolu <boluwatifetoluwalase13@gmail.com>
2025-05-16 19:46:35 -0700

    api

--- Script/post.js
+++ Script/post.js
@@ -1,5 +1,24 @@
 function.getPost() {
     let blogId = localStorage.getItem("blogId")
 
-    
+    fetch('http')
+    .then(response => response.json())
+    .then((data)) => {
+        let postBody = document.getElementById("post-body")
+
+        let blogDetails = ""
+
+        blogDetails += `
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="mb-4">
+                        ${data.title}
+                    </h1>
+                    <p>${data.body}</p>
+                </div>
+            </div>
+            
+        `
+    }
+
 }
