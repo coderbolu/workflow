823e7aa7b979a3bfc91313325961b00c1b2c9a6b
coderbolu <boluwatifetoluwalase13@gmail.com>
2025-05-16 19:21:24 -0700

    api

--- Script/api.js
+++ Script/api.js
@@ -1,3 +1,18 @@
 let postTitle = document.getElementById("post-title")
 let postBody = document.getElementById("post-body")
 let postForm = document.getElementById("post-form")
+
+let newArray = []
+
+
+//////Fetch blog post
+function getPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => {
+            newArray = data
+            addpost()
+    })
+
+    console.log(newArray)
+}
